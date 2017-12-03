73ab8f9480fdd75e10d5d175adf55c7bafb624d8
husnamustapha <32616957+husnamustapha@users.noreply.github.com>
2017-12-03 09:43:24 +0800

    Add files via upload

---
+++ clientgo.go
@@ -0,0 +1,25 @@
+package main
+
+import "net"
+
+import "fmt"
+
+import "bufio"
+
+import "os"
+
+
+
+func main() {
+  
+  conn, _ := net.Dial("tcp",192.168.94.129",7777) 
+  
+  reader := bufio.NewReader(os.Stdin)
+  
+  fmt.Print("Text to send: ")
+  
+  text, _ := reader.ReadString('\n')
+    
+  fmt.Fprintf(conn, text + "\n")
+
+}
---
+++ loadbalancer.go
@@ -0,0 +1,67 @@
+package main
+
+
+import "net"
+
+
+import "fmt"
+
+
+import "bufio"
+
+
+import "os"
+
+
+
+
+func main() {
+
+
+  fmt.Println("Launching server...")
+
+  
+  
+  ln, _ := net.Listen("tcp", ":7777")
+
+  
+  
+  conn, _ := ln.Accept()
+
+ 
+  
+  if(clientsentence.contains("morning")) {	
+    
+    conn, _ := net.Dial("tcp","192.168.80.133",8888)  
+    
+    reader := bufio.NewReader(os.Stdin)
+    
+    fmt.Print("Text to send: ")
+  
+    
+    text, _ := reader.ReadString('\n')
+    
+    
+    fmt.Fprintf(conn, text + "\n")
+
+  
+  }
+  
+  else if(clientsentence.contains("night")) {	
+    
+    conn2, _ := net.Dial("tcp","192.168.80.136",9999)  
+    
+    reader := bufio.NewReader(os.Stdin)
+ 
+
+    fmt.Print("Text to send: ")
+ 
+
+    text, _ := reader.ReadString('\n')
+    
+    
+    fmt.Fprintf(conn2, text + "\n")
+
+   
+  }
+}
